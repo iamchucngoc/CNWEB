f4439d03988542cd3985805231fe1adc0464fd11
ngoccn <ngochucngoc@gmail.com>
2024-11-25 21:08:07 +0700

    homework 25.11 and btth1

---
+++ BTVN/25_11/footer.php
@@ -0,0 +1,33 @@
+<!-- Thêm đường kẻ ngang trước footer nếu muốn -->
+<hr style="border: 1px solid #ddd; margin: 20px 0;">
+<div class="clearfix">
+    <div class="hint-text" style="text-align: center; font-weight: bold; font-size: 20px;">TLU'S MUSIC GARDEN</div>
+</div>
+
+<!-- <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Select/Deselect checkboxes
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $("#selectAll").click(function(){
+        if(this.checked){
+            checkbox.each(function(){
+                this.checked = true;                        
+            });
+        } else{
+            checkbox.each(function(){
+                this.checked = false;                        
+            });
+        } 
+    });
+    checkbox.click(function(){
+        if(!this.checked){
+            $("#selectAll").prop("checked", false);
+        }
+    });
+});
+</script> -->
+</body>
+
+</html>
---
+++ BTVN/25_11/index.php
@@ -0,0 +1,5 @@
+<?php
+    include('header.php');  // Include header part
+    include('main.php');    // Include main content part
+    include('footer.php');  // Include footer part
+?>
